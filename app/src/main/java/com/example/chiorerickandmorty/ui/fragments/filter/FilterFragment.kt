26debad14868492ff6bcf183a79d47832ffad097
KotlinCountriesTest <elmarshkrv@gmail.com>
2023-05-15 14:17:58 +0400

    filter bottom fragment

---
+++ app/src/main/java/com/example/chiorerickandmorty/ui/fragments/filter/FilterFragment.kt
@@ -0,0 +1,29 @@
+package com.example.chiorerickandmorty.ui.fragments.filter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chiorerickandmorty.databinding.FragmentFilterBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FilterFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentFilterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFilterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/chiorerickandmorty/ui/fragments/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/chiorerickandmorty/ui/fragments/homefragment/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
@@ -44,6 +45,10 @@ class HomeFragment : Fragment() {
 
         setUpHomeRv()
         initAdapter()
+
+        binding.filterIv.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_filterFragment)
+        }
 
     }
 
