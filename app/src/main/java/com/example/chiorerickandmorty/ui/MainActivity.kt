68c92dc2ed8fed6a78722b2fadc90999f7a2cfc1
KotlinCountriesTest <elmarshkrv@gmail.com>
2023-05-18 17:51:04 +0400

    disable dark mode and edit filter fragment

--- app/src/main/java/com/example/chiorerickandmorty/ui/MainActivity.kt
+++ app/src/main/java/com/example/chiorerickandmorty/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.chiorerickandmorty.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -22,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
 
         val navHostFragment =
